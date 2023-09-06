cd2955abe13d1ca69dee6ab3b4560fcaf50131de
nihalmohd <nihalmohd828@gmail.com>
2023-09-06 12:02:42 +0530

    whishlist Design is started

--- src/Components/User/Whishlist/Userwhishlisht.tsx
+++ src/Components/User/Whishlist/Userwhishlisht.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Userwhishlisht = () => {
   return (
-    <div>Userwhishlisht</div>
+    <div>
+        <div className="w-full h-screen bg-red-100 p-1">
+            <div className="w-full h-32 bg-yellow-100"></div>
+        </div>
+    </div>
   )
 }
 
